4d11c7549194bee7f7900626cdb5cd21b5aac562
Ross van Zyl <rossvz@gmail.com>
2015-11-28 15:47:45 -0500

    front end scaffold with yeoman

---
+++ public/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name brocodiusApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the brocodiusApp
+ */
+angular.module('brocodiusApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ public/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name brocodiusApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the brocodiusApp
+ */
+angular.module('brocodiusApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
